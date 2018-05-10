344ae76187249490f2e9109ce9fb83afd58f210a
Jake Pizzala <jake@kiva.org>
2018-05-10 11:55:41 -0700

    initial commit

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	render_template("homepage.html")
+	return render_template("homepage.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
